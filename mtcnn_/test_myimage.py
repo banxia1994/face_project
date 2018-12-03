38b496830d172caa2594b9401be530aa96d9f2e0
banxia1994 <609510481@qq.com>
2018-12-03 12:17:58 +0800

    mtcnn

---
+++ mtcnn_/test_myimage.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+
+from src import detect_faces,show_bboxes
+from PIL import Image
+
+img = Image.open('images/office1.jpg')
+bounding_boxes,landmarks = detect_faces(img)
+img = show_bboxes(img,bounding_boxes,landmarks)
+img.save('a.jpg')
